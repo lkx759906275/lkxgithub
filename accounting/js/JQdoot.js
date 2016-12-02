1516ca66ddb748b2dfd9c0eb4d7886aaa3fb0fc7
e <apple.mst@qq.com>
2016-12-02 10:55:16 +0800

    huiji

---
+++ accounting/js/JQdoot.js
@@ -0,0 +1,5 @@
+//多文本溢出
+$(document).ready(function() {  
+            $(".p-n2").dotdotdot();      
+            
+});
---
+++ accounting/js/super.js
@@ -0,0 +1,141 @@
+ // 轮播图
+     var swiper = new Swiper('.swiper-container', {
+         pagination: '.swiper-pagination',
+         nextButton: '.swiper-button-next',
+         prevButton: '.swiper-button-prev',
+         slidesPerView: 1,
+         paginationClickable: true,
+         spaceBetween: 30,
+         loop: true,
+         centeredSlides: true,
+    	 autoplay: 2500,
+     	 autoplayDisableOnInteraction: false,
+     	 autoHeight: true //enable auto height
+     });
+ 
+ // 展示图js轮播
+    jQuery.fn.extend({
+        pic_scroll:function (){
+            $(this).each(function(){
+                var _this=$(this);//存储对象
+                var ul=_this.find("ul");//获取ul对象
+                var li=ul.find("li");//获取所有图片所有的li
+                var w=li.size()*li.width();//统计图片的长度
+                li.clone().prependTo(ul);//克隆图片一份放入ul里
+                ul.width(2*w);//设置ul的长度，使所有图片排成一排
+                var i=1,l;
+                _this.hover(function(){i=0},function(){i=1});//鼠标经过时设置i=0达到鼠标经过停止效果
+                setInterval(function(){
+                    //定时滚动函数
+                    l = _this.scrollLeft();
+                    if (l < w) {
+                        _this.scrollLeft(l+i);
+                    } else {
+                        _this.scrollLeft(0);
+                    }
+                },20);
+            })
+        }
+    });
+    $(document).ready(function(){
+        $(".superman-list").pic_scroll();//多个地方使用
+    })
+
+// 地图
+
+    //创建和初始化地图函数：
+    function initMap(){
+        createMap();//创建地图
+        setMapEvent();//设置地图事件
+        addMapControl();//向地图添加控件
+        addMarker();//向地图中添加marker
+    }
+    
+    //创建地图函数：
+    function createMap(){
+        var map = new BMap.Map("dituContent");//在百度地图容器中创建一个地图
+        var point = new BMap.Point(114.90018,40.76932);//定义一个中心点坐标
+        map.centerAndZoom(point,18);//设定地图的中心点和坐标并将地图显示在地图容器中
+        window.map = map;//将map变量存储在全局
+    }
+    
+    //地图事件设置函数：
+    function setMapEvent(){
+        map.enableDragging();//启用地图拖拽事件，默认启用(可不写)
+        map.enableScrollWheelZoom();//启用地图滚轮放大缩小
+        map.enableDoubleClickZoom();//启用鼠标双击放大，默认启用(可不写)
+        map.enableKeyboard();//启用键盘上下左右键移动地图
+    }
+    
+    //地图控件添加函数：
+    function addMapControl(){
+        //向地图中添加缩放控件
+    var ctrl_nav = new BMap.NavigationControl({anchor:BMAP_ANCHOR_TOP_LEFT,type:BMAP_NAVIGATION_CONTROL_LARGE});
+    map.addControl(ctrl_nav);
+        //向地图中添加缩略图控件
+    var ctrl_ove = new BMap.OverviewMapControl({anchor:BMAP_ANCHOR_BOTTOM_RIGHT,isOpen:1});
+    map.addControl(ctrl_ove);
+        //向地图中添加比例尺控件
+    var ctrl_sca = new BMap.ScaleControl({anchor:BMAP_ANCHOR_BOTTOM_LEFT});
+    map.addControl(ctrl_sca);
+    }
+    
+    //标注点数组
+    var markerArr = [{title:"张家口广升会计师事务所",content:"张家口市高新区6号辽海国际2-6C",point:"114.899619|40.769334",isOpen:0,icon:{w:21,h:21,l:0,t:0,x:6,lb:5}}
+         ];
+    //创建marker
+    function addMarker(){
+        for(var i=0;i<markerArr.length;i++){
+            var json = markerArr[i];
+            var p0 = json.point.split("|")[0];
+            var p1 = json.point.split("|")[1];
+            var point = new BMap.Point(p0,p1);
+            var iconImg = createIcon(json.icon);
+            var marker = new BMap.Marker(point,{icon:iconImg});
+            var iw = createInfoWindow(i);
+            var label = new BMap.Label(json.title,{"offset":new BMap.Size(json.icon.lb-json.icon.x+10,-20)});
+            marker.setLabel(label);
+            map.addOverlay(marker);
+            label.setStyle({
+                        borderColor:"#808080",
+                        color:"#333",
+                        cursor:"pointer"
+            });
+            
+            (function(){
+                var index = i;
+                var _iw = createInfoWindow(i);
+                var _marker = marker;
+                _marker.addEventListener("click",function(){
+                    this.openInfoWindow(_iw);
+                });
+                _iw.addEventListener("open",function(){
+                    _marker.getLabel().hide();
+                })
+                _iw.addEventListener("close",function(){
+                    _marker.getLabel().show();
+                })
+                label.addEventListener("click",function(){
+                    _marker.openInfoWindow(_iw);
+                })
+                if(!!json.isOpen){
+                    label.hide();
+                    _marker.openInfoWindow(_iw);
+                }
+            })()
+        }
+    }
+    //创建InfoWindow
+    function createInfoWindow(i){
+        var json = markerArr[i];
+        var iw = new BMap.InfoWindow("<b class='iw_poi_title' title='" + json.title + "'>" + json.title + "</b><div class='iw_poi_content'>"+json.content+"</div>");
+        return iw;
+    }
+    //创建一个Icon
+    function createIcon(json){
+        var icon = new BMap.Icon("http://app.baidu.com/map/images/us_mk_icon.png", new BMap.Size(json.w,json.h),{imageOffset: new BMap.Size(-json.l,-json.t),infoWindowOffset:new BMap.Size(json.lb+5,1),offset:new BMap.Size(json.x,json.h)})
+        return icon;
+    }
+    
+    initMap();//创建和初始化地图
+
